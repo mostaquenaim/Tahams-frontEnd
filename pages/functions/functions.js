0fd6100f75839a4e8a92c8f76f79c4fbc93d8dcd
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2023-12-09 17:25:22 +0600

    add product

--- pages/functions/functions.js
+++ pages/functions/functions.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 import Link from "next/link";
-// export const ListStyle = ({ goto, pageName }) => {
-//     return (
-//         <li className='hover:text-zinc-400 md:border-b-2 border-transparent hover:border-zinc-600 '>
-//             <Link href={goto}>{pageName}</Link>
-//             <hr className="hidden hover:inline-block w-full border-black duration-300 transition-width"></hr>
-//         </li>
-// // hidden hover:inline-block
-//     );
-// }
 
-export const loadCategories = async () => {
+ const loadCategories = async () => {
     try {
         const resp = await axios.get('/categories.json');
         return resp.data;
@@ -19,3 +10,5 @@ export const loadCategories = async () => {
         throw error;
     }
 };
+
+export default loadCategories
