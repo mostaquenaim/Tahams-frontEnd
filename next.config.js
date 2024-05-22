3d29ef5d6ed85377e51e93ba6703f9cc4a5e1504
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2024-05-22 17:45:41 +0600

    next config updated

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'https://api.tahamsbd.com',
+        hostname: 'api.tahamsbd.com',
         port: '3000',
       },
     ],
