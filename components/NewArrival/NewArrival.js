54011b482047af7d48d12ea9298b6d2d0929afad
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2024-12-05 13:29:04 +0600

    new-arrivals

--- components/NewArrival/NewArrival.js
+++ components/NewArrival/NewArrival.js
@@ -14,7 +14,7 @@ const NewArrival = () => {
         <>
             <div className="pt-20 md:pt-16 lg:pt-10 shadow-md px-10">
                 <Heading first='NEW' second='ARRIVALS'></Heading>
-                <div className="pt-10 pb-20 grid grid-cols-1 md:grid-cols-3 gap-5">
+                <div className="pt-10 pb-20 grid grid-cols-2 md:grid-cols-4 gap-5">
                     {
                         products.map((product, index) => (
                             <ShowNewArrival key={index} prop={product}></ShowNewArrival>
--- components/NewArrival/ShowNewArrival.js
+++ components/NewArrival/ShowNewArrival.js
@@ -1,11 +1,12 @@
+import Link from "next/link";
 
 const ShowNewArrival = ({ prop }) => {
     return (
         <>
             <div className='relative '>
-                <figure className='relative '>
+                <Link href={`/categories/${prop.category}`} className='relative '>
                     <img src={prop.image} className='rounded-lg shadow-md hover:scale-105 duration-1000 -z-50' alt={prop.name} />
-                </figure>
+                </Link>
             </div>
         </>
     );
