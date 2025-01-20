6873dec127ca529ede1c258fbc193745a79f6ac8
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2025-01-20 16:47:03 +0600

    bkash number update

--- components/Cart/PaymentInfo.js
+++ components/Cart/PaymentInfo.js
@@ -20,7 +20,7 @@ const PaymentInfo = ({ history }) => {
 
     const paymentMethods = [
         { id: 1, name: 'Cash on Delivery', icon: '/cod.png', process: 'Payment upon delivery' },
-        { id: 2, name: 'BKash', icon: '/bkash.png', process: 'Merchant Number: 01602054102 (PAYMENT)\nReference: Your name and amount you pay' },
+        { id: 2, name: 'BKash', icon: '/bkash.png', process: 'Merchant Number: 01314188605 (PAYMENT)\nReference: Your name and amount you pay' },
         { id: 3, name: 'Nagad', icon: '/nagad.png', process: 'Merchant Number: 01602054102 (PAYMENT)\nReference: Your name and amount you pay' },
         { id: 4, name: 'Rocket', icon: '/rocket.png', process: 'Number: 01602054102 (Send Money)\nReference: Your name and amount you pay' },
         { id: 5, name: 'Visa Card', icon: '/visa.png', process: 'Please proceed with your Visa Card details.\n\nBank Name: The City Bank Ltd.\nBranch Name: Shyamoli\nA/C NAME: TAHAMS\nA/C NUMBER: 1233871270001' },
