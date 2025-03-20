a4083e2f44bf08d59979debf7f856b26123b6e7e
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2025-03-20 12:50:40 +0600

    window location fixed

--- pages/search-product/index.js
+++ pages/search-product/index.js
@@ -2,20 +2,22 @@ import Loading from '/components/Loading';
 import FetchProducts from '/components/Product/FetchProducts';
 import useAxiosPublic from '/Hooks/useAxiosPublic';
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const SearchProduct = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
 
-    const axiosPublic = useAxiosPublic()
+    const axiosPublic = useAxiosPublic();
+    const router = useRouter();
 
     useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const searchQuery = urlParams.get('search');
-        // console.log(searchQuery);
-        setSearchQuery(searchQuery);
-    }, [window.location.search]);
+        const { search } = router.query; // Access the query param `search` from URL
+        if (search) {
+            setSearchQuery(search);
+        }
+    }, [router.query]); // Trigger when the URL query changes
 
     useEffect(() => {
         if (searchQuery) {
@@ -24,24 +26,20 @@ const SearchProduct = () => {
     }, [searchQuery]);
 
     const fetchProducts = async (searchQuery) => {
-        setIsLoading(true)
+        setIsLoading(true);
         try {
             const response = await axiosPublic.get(`admin/search-products?q=${searchQuery}`);
-            console.log(response.data);
             setProducts(response.data);
         } catch (error) {
             console.error(error);
-        }
-        finally{
-            setIsLoading(false)
+        } finally {
+            setIsLoading(false);
         }
     };
 
     return (
         <div>
-            {
-                <FetchProducts categories={products} query={searchQuery} isLoading={isLoading} />
-            }
+            <FetchProducts categories={products} query={searchQuery} isLoading={isLoading}/>
         </div>
     );
 };
