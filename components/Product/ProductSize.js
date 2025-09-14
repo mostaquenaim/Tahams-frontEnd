5d3214f988d2dbfc13ef303d02cab042bdd37d49
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2025-09-14 18:09:16 +0600

    get back to old sizes

--- components/Product/ProductSize.js
+++ components/Product/ProductSize.js
@@ -1,4 +1,3 @@
-import useLoadSizes from '/Hooks/useLoadSizes';
 import React from 'react';
 
 const ProductSize = ({
@@ -9,58 +8,48 @@ const ProductSize = ({
   selectedFemaleSize,
   handleFemaleSizeChange,
 }) => {
-  const sizes = useLoadSizes();
-
   const filteredSizes = selectedCategory
-    ? product.pscs.map((p) => ({
-        size: p.size,
-        quantity: p.quantity,
-      }))
+    ? product.pscs
+        .filter((p) => p.category.id === selectedCategory && p.quantity > 0)
+        .map((p) => p.size)
     : [];
-
   return (
     <div>
       {filteredSizes && filteredSizes.length > 0 && (
         <div className="mb-4">
           <label className="text-gray-600 font-semibold">Select Size:</label>
-          {product.pscs[0].category.category.category.name === 'Couples' && (
+          {product.pscs[0].category.category.category.name == 'Couples' && (
             <p className="text-gray-600 p-2 text-xl font-semibold">Male:</p>
           )}
           <div className="flex gap-3 flex-wrap">
-            {filteredSizes.map(({ size, quantity }) => (
+            {filteredSizes.map((size) => (
               <button
                 key={size?.id}
                 className={`btn btn-outline ${
                   selectedSize === size?.name
                     ? 'bg-black text-white'
                     : 'bg-white text-black'
-                } border-black text-black ${
-                  quantity === 0 ? 'btn-disabled' : ''
-                }`}
-                onClick={() => quantity > 0 && handleSizeChange(size?.name)}
+                } border-black text-black`}
+                onClick={() => handleSizeChange(size?.name)}
               >
                 {size?.name}
               </button>
             ))}
           </div>
-
-          {product.pscs[0].category.category.category.name === 'Couples' && (
+          {product.pscs[0].category.category.category.name == 'Couples' && (
             <div>
               <p className="text-gray-600 p-2 text-xl font-semibold">Female:</p>
+
               <div className="flex gap-3 flex-wrap">
-                {filteredSizes.map(({ size, quantity }) => (
+                {filteredSizes.map((size) => (
                   <button
                     key={size?.id}
                     className={`btn btn-outline ${
                       selectedFemaleSize === size?.name
                         ? 'bg-black text-white'
                         : 'bg-white text-black'
-                    } border-black text-black ${
-                      quantity === 0 ? 'btn-disabled' : ''
-                    }`}
-                    onClick={() =>
-                      quantity > 0 && handleFemaleSizeChange(size?.name)
-                    }
+                    } border-black text-black`}
+                    onClick={() => handleFemaleSizeChange(size?.name)}
                   >
                     {size?.name}
                   </button>
--- pages/products/details/[id].js
+++ pages/products/details/[id].js
@@ -534,58 +534,6 @@ const Product = ({ product }) => {
             </div>
 
             {/* Size Selection */}
-            {/* {filteredSizes && filteredSizes.length > 0 && (
-              <div className="mb-4">
-                <label className="text-gray-600 font-semibold">
-                  Select Size:
-                </label>
-                {product.pscs[0].category.category.category.name ==
-                  'Couples' && (
-                  <p className="text-gray-600 p-2 text-xl font-semibold">
-                    Male:
-                  </p>
-                )}
-                <div className="flex gap-3 flex-wrap">
-                  {filteredSizes.map((size) => (
-                    <button
-                      key={size?.id}
-                      className={`btn btn-outline ${
-                        selectedSize === size?.name
-                          ? 'bg-black text-white'
-                          : 'bg-white text-black'
-                      } border-black text-black`}
-                      onClick={() => handleSizeChange(size?.name)}
-                    >
-                      {size?.name}
-                    </button>
-                  ))}
-                </div>
-                {product.pscs[0].category.category.category.name ==
-                  'Couples' && (
-                  <div>
-                    <p className="text-gray-600 p-2 text-xl font-semibold">
-                      Female:
-                    </p>
-
-                    <div className="flex gap-3 flex-wrap">
-                      {filteredSizes.map((size) => (
-                        <button
-                          key={size?.id}
-                          className={`btn btn-outline ${
-                            selectedFemaleSize === size?.name
-                              ? 'bg-black text-white'
-                              : 'bg-white text-black'
-                          } border-black text-black`}
-                          onClick={() => handleFemaleSizeChange(size?.name)}
-                        >
-                          {size?.name}
-                        </button>
-                      ))}
-                    </div>
-                  </div>
-                )}
-              </div>
-            )} */}
             <ProductSize
               selectedCategory={selectedCategory}
               product={product}
