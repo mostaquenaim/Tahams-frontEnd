08f50eca7cf047f39b3a6a7fb235ede7f3a4722e
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2025-12-19 15:27:00 +0600

    show all cart redirect

--- components/Drawers/AdminDrawer.js
+++ components/Drawers/AdminDrawer.js
@@ -203,7 +203,7 @@ const AdminDrawer = () => {
           ),
         },
         { href: '/admin/show/show-requests', label: 'Cancel/Return Requests' },
-        { href: '/MyCart', label: 'View Carts' },
+        { href: '/admin/show/show-all-carts', label: 'View Carts' },
       ],
     },
     {
